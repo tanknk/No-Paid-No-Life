5b8cfce631070be6b46a75e39d426861504d279d
kesaraneet <kesaraneet@gmail.com>
2019-04-14 21:17:44 +0700

    update

--- test/main.c
+++ test/main.c
@@ -11,14 +11,31 @@ main(){
     int range = 10 , fail_login = 1;
     char id[range], pass[range], buffer[range];
     char ch; // ตัวแปรเก็บแต่ละตัวของพาสเวิร์ด
-    int passposition = 0; //นับจำนวนพาสเวิร์ดที่กรอกเข้ามา
+    int passposition = 0, answer = 0; //นับจำนวนพาสเวิร์ดที่กรอกเข้ามา
     printf("ENTER ID :");
     scanf("%s", id);
     printf("ENTER PASSWORD :");
     scanf("%s", pass);
     fscanf(log, "%s", buffer);
-    printf("THIS IS BUFFER > %s\n", buffer);
-    fscanf(log, "%s", buffer);
-    printf("THIS IS BUFFER > %s\n", buffer);
+    // fscanf(log, "%s", buffer);
+    // printf("THIS IS BUFFER > %s\n", buffer);
+    // fscanf(log, "%s", buffer);
+    // printf("THIS IS BUFFER > %s\n", buffer);
+    // fscanf(log, "%s", buffer);
+    // printf("THIS IS BUFFER > %s\n", buffer);
+    while (strcmp(buffer, "end") != 0){
+        fscanf(log, "%s", buffer);
+        // printf("THIS IS BUFFER > %s\n", buffer);
+        // printf("buffer is %s and ID is %s\n", buffer, id);
+        if (strcmp(buffer, id) == 0){
+            answer = 1;
+        }
+    }
+    if(answer==1){
+        printf("success");
+    }
+    else{
+        printf("failed");
+    }
     fclose(log);
 }
