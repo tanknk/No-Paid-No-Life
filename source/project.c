d5bc9338e7f8ca3c9d00a20411c172e4f0a1f2c8
tanknk <42908785+tanknk@users.noreply.github.com>
2019-04-15 13:51:03 +0700

    update graphic

--- source/project.c
+++ source/project.c
@@ -1,13 +1,34 @@
 #include "stdio.h"
 #include "string.h"
 #include "conio.h"
-
+int intro(); //ฟังก์ชั่น Intro
 int id();    //ฟังก์ชั่นเช็คว่ามี ID อยู่ในฐานข้อมูลมั้ย
 int pass();  //ฟังก์ชั่นเช็คว่ามี Password อยู่ในฐานข้อมูลมั้ย
 int regis(); // ฟังก์ชั่นสมัครสมาชิก
 
 int main(){
-    id();
+    intro();
+}
+
+int intro(){
+    int decision;
+    printf("+----------------------------------+\n");
+    printf(":    WELCOME TO NO PAID NO LIFE    :\n");
+    printf(": Put 1 If this's your first time  :\n");
+    printf(": Put 2 If you have an account     :\n");
+    printf(": Put 3 if you want to shut down   :\n");
+    printf("+----------------------------------+\n");
+    printf("Your Choice : ");
+    scanf("%d", &decision);
+    while(decision > 3){
+        printf("Wrong Command, Try Again pls\n");
+        scanf("%d", &decision);
+    }
+    switch(decision){
+        case 1: regis(); break;
+        case 2: id(); break;
+        case 3: return 0; break;
+    }
 }
 
 int id(){
@@ -15,7 +36,7 @@ int id(){
     id_csv = fopen("../data/id.csv", "a+");
     int check = 0, decision;
     char id[10], buffer_id[10];
-    printf("ENTER ID :");
+    printf("ENTER ID : ");
     scanf("%s", id);
     fscanf(id_csv, "%s", buffer_id);
     while(strcmp(buffer_id, "END") != 0){
@@ -53,7 +74,7 @@ int pass(){
     pass_csv = fopen("../data/pass.csv", "a+");
     int check = 0;
     char password[10], buffer_pass[10];
-    printf("ENTER PASSWORD :");
+    printf("ENTER PASSWORD : ");
     scanf("%s", password);
     fscanf(pass_csv, "%s", buffer_pass);
     while(strcmp(buffer_pass, "END") != 0){
@@ -77,19 +98,17 @@ int regis(){
     FILE *regis_idcsv;
     regis_idcsv = fopen("../data/id.csv", "a+");
     char id_regis[10];
-    printf("Your ID: ");
+    printf("Your ID : ");
     scanf("%s", id_regis);
-    fprintf(regis_idcsv, "%s\n", id_regis);
+    fprintf(regis_idcsv, "%s", id_regis);
     fclose(regis_idcsv);
     FILE *regis_passcsv;
     regis_passcsv = fopen("../data/pass.csv", "a+");
     char pass_regis[10];
-    printf("Your Password: ");
+    printf("Your Password : ");
     scanf("%s", pass_regis);
-    fprintf(regis_passcsv, "%s\n", pass_regis);
+    fprintf(regis_passcsv, "%s", pass_regis);
     printf("REGISTER SUCCESSFUL!\n");
+    printf("PLEASE LOG IN AGAIN !\n");
     main();
-    //printf("CURRENT ID IS : %s\n", id_regis);
-    //printf("CURRENT PASSWORD IS : %s\n", pass_regis);
-    //printf("PLEASE LOG IN AGAIN !\n");
 }
